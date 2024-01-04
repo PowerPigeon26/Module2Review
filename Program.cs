7366b22fdc78ba3865ac9a18d31c411eccf4451c
Maverick Grayer <maverick72032@gmail.com>
2024-01-04 13:16:53 -0500

    Module 2 practice done

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Module2Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            Student stacy = new Student("Stacy", 1);
+            students.Add(stacy);
+            stacy.AddGrade(100.0);
+            stacy.AddGrade(100.0, 100.0, 89.0);
+
+            Student lucy = new Student("Lucy", 2);
+            students.Add(lucy);
+            lucy.AddGrade(65);
+            lucy.AddGrade(60, 75, 72);
+
+            Student baxter = new Student("Baxter", 3);
+            students.Add(baxter);
+            baxter.AddGrade(90);
+            baxter.AddGrade(85, 70, 85);
+
+            Student vertin = new Student("Vertin", 4);
+            students.Add(vertin);
+            vertin.AddGrade(100);
+            vertin.AddGrade(100, 100, 0);
+
+            // used string concatenation rather than string interpolation
+            Console.WriteLine("---------------------------------------------------------------------------");
+            foreach (Student student in students)
+            {
+                Console.WriteLine("Name: " + student.Name);
+                Console.WriteLine("ID: " + student.ID);
+                Console.Write("Grades: ");
+                foreach (double grade in student.Grades)
+                {
+                    Console.Write(grade + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Average grade: " + student.CalculateAverageGrade());
+                Console.WriteLine("---------------------------------------------------------------------------");
+            }
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Review
+{
+    public class Student
+    {
+        public string Name { get; set; } = "";
+        public int ID { get; set; }
+        public List<double> Grades { get; set; } = new List<double>();
+
+        public Student() { }
+
+        public Student(string name, int iD)
+        {
+            Name = name;
+            ID = iD;
+        }
+
+        // not sure why instructions say all methods will be static, I'm not sure how the methods would work in that case,
+        // not without changing the methods to take in a instance of student and modify it. I choose to use non-static methods.
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public void AddGrade(params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+
+        public double CalculateAverageGrade()
+        {
+            if (Grades.Count == 0) return 0;
+            else return Grades.Average();
+        }
+
+    }
+}
